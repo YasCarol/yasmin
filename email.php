3e00901ccfc8a82741ed5c2bb5992c92a1965d94
YasCarol <carolineyasmin815@gmail.com>
2021-11-04 21:15:22 -0300

    UPDATE

--- email.php
+++ email.php
@@ -1,26 +1,24 @@
-
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
-
 $mail = new PHPMailer;
 $mail->isSMTP();
 $mail->SMTPDebug = 2;
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = 'smtp.office365.com';
 $mail->Port = 587;
 $mail->SMTPAuth = true;
 $mail->Username = 'carolineyasmin815@gmail.com';
 $mail->Password = 'yasmin123456';
-$mail->setFrom('yasmin@azapfy.com', 'yas');
-//$mail->addReplyTo('kleberroro23@gmail.com', 'kleber');
-$mail->addAddress('Kleberroro23@gmail.com', 'Elmo');
-$mail->Subject = 'Bom diaa Elmixxx';
-//$mail->msgHTML(file_get_contents('message.html'), _DIR_);
-$mail->Body = " Ontem eu dormi em.. mas hoje foiii";
+$mail->setFrom('carolineyasmin815@gmail.com', 'yas');
+//$mail->addReplyTo('test@hostinger-tutorials.com', 'Your Name');
+$mail->addAddress('yasminazapfy@gmail.com', 'Receiver Name');
+$mail->Subject = 'Teste';
+//$mail->msgHTML(file_get_contents('message.html'), __DIR__);
+$mail->Body = 'TESTE';
 $mail->addAttachment('../pdf/exercicios.pdf');
 if (!$mail->send()) {
-    echo 'Sucesso' . $mail->ErrorInfo;
+    echo 'EMAIL NÂO ENVIADO' . $mail->ErrorInfo;
 } else {
-    echo 'Falha';
+    echo 'EMAIL ENVIADO';
 }
